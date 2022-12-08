9f8416ab752277faf9ec67db0be34b1ff6c3377b
Kent Quirk <kentquirk@gmail.com>
2022-12-07 22:43:55 -0500

    Solve part 1 of day 7

---
+++ day07/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type FileTree struct {
+	Name   string
+	Dirs   map[string]*FileTree
+	Files  map[string]int
+	Parent *FileTree
+}
+
+func NewFileTree(name string, parent *FileTree) *FileTree {
+	return &FileTree{
+		Name:   name,
+		Dirs:   make(map[string]*FileTree),
+		Files:  make(map[string]int),
+		Parent: parent,
+	}
+}
+
+func (f *FileTree) Print(indent int) {
+	fmt.Printf("%s- %s (dir, size = %d)\n", strings.Repeat(" ", indent*2), f.Name, f.Size())
+	for _, d := range f.Dirs {
+		d.Print(indent + 2)
+	}
+	for name, size := range f.Files {
+		fmt.Printf("  %s- %s (file, size=%d)\n", strings.Repeat(" ", indent*2), name, size)
+	}
+}
+
+func (f *FileTree) Size() int {
+	t := 0
+	for _, d := range f.Dirs {
+		t += d.Size()
+	}
+	for _, size := range f.Files {
+		t += size
+	}
+	return t
+}
+
+func (f *FileTree) Walk(functor func(f *FileTree)) {
+	functor(f)
+	for _, d := range f.Dirs {
+		d.Walk(functor)
+	}
+}
+
+func (f *FileTree) Path() string {
+	if f.Parent == nil {
+		return f.Name
+	}
+	return f.Parent.Path() + f.Name + "/"
+}
+
+type Parser struct {
+	ix       int
+	cmds     []string
+	pushback []string
+}
+
+func NewParser(lines []string) *Parser {
+	return &Parser{cmds: lines}
+}
+
+func (p *Parser) Pushback(sa []string) {
+	p.pushback = sa
+}
+
+func (p *Parser) Get() []string {
+	if p.pushback != nil {
+		r := p.pushback
+		p.pushback = nil
+		return r
+	}
+	if p.ix >= len(p.cmds) {
+		return nil
+	}
+
+	splits := strings.Split(p.cmds[p.ix], " ")
+	p.ix++
+	if len(splits) < 2 {
+		return nil
+	}
+	return splits
+}
+
+func parse(lines []string) *FileTree {
+	p := NewParser(lines)
+	root := NewFileTree("/", nil)
+	current := root
+
+outer:
+	for cmd := p.Get(); cmd != nil; cmd = p.Get() {
+		if cmd[0] == "$" {
+			switch cmd[1] {
+			case "cd":
+				switch cmd[2] {
+				case "/":
+					current = root
+				case "..":
+					if current.Parent != nil {
+						current = current.Parent
+					}
+				default:
+					if sub, ok := current.Dirs[cmd[2]]; ok {
+						current = sub
+					}
+				}
+			case "ls":
+				for {
+					item := p.Get()
+					if item == nil || item[0] == "$" {
+						p.Pushback(item)
+						continue outer
+					}
+					if item[0] == "dir" {
+						current.Dirs[item[1]] = NewFileTree(item[1], current)
+					} else {
+						sz, _ := strconv.Atoi(item[0])
+						current.Files[item[1]] = sz
+					}
+				}
+			}
+		}
+	}
+	return root
+}
+
+type totaller struct {
+	total int
+}
+
+func (t *totaller) walker(f *FileTree) {
+	if f.Size() <= 100000 {
+		t.total += f.Size()
+	}
+}
+
+func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(b), "\n")
+	root := parse(lines)
+	root.Print(0)
+	var tt totaller
+	root.Walk(tt.walker)
+	fmt.Println(tt)
+}
