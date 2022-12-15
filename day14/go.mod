56568d12908761bc2d2714e13c08af3437706d0a
Kent Quirk <kentquirk@honeycomb.io>
2022-12-14 20:35:44 -0500

    Day 14, part 1

---
+++ day14/go.mod
@@ -0,0 +1,3 @@
+module github.com/kentquirk/aoc2022/day14
+
+go 1.19
---
+++ day14/main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type State int
+
+const (
+	Empty State = iota
+	Wall
+	Sand
+	Origin
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Cave struct {
+	Min   Point
+	Max   Point
+	Cells map[Point]State
+}
+
+func NewCave() *Cave {
+	return &Cave{
+		Min:   Point{10000, 10000},
+		Max:   Point{0, 0},
+		Cells: make(map[Point]State),
+	}
+}
+
+func (c *Cave) Print() {
+	fmt.Println("---")
+	for y := c.Min.Y; y <= c.Max.Y; y++ {
+		for x := c.Min.X; x <= c.Max.X; x++ {
+			switch c.Cells[Point{x, y}] {
+			case Empty:
+				fmt.Print(".")
+			case Wall:
+				fmt.Print("#")
+			case Sand:
+				fmt.Print("o")
+			case Origin:
+				fmt.Print("+")
+			default:
+				fmt.Print("!")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func sign(v1, v2 int) int {
+	switch {
+	case v1 < v2:
+		return 1
+	case v1 > v2:
+		return -1
+	default:
+		return 0
+	}
+}
+
+func (c *Cave) CheckLimits(pt Point) {
+	if pt.X < c.Min.X {
+		c.Min.X = pt.X
+	}
+	if pt.X > c.Max.X {
+		c.Max.X = pt.X
+	}
+	if pt.Y < c.Min.Y {
+		c.Min.Y = pt.Y
+	}
+	if pt.Y > c.Max.Y {
+		c.Max.Y = pt.Y
+	}
+}
+
+func (c *Cave) DrawWall(from Point, to Point) {
+	c.CheckLimits(from)
+	c.CheckLimits(to)
+	if from.Y == to.Y {
+		sgn := sign(from.X, to.X)
+		for x := from.X; x != to.X+sgn; x += sgn {
+			c.Cells[Point{x, from.Y}] = Wall
+		}
+	} else {
+		sgn := sign(from.Y, to.Y)
+		for y := from.Y; y != to.Y+sgn; y += sgn {
+			c.Cells[Point{from.X, y}] = Wall
+		}
+	}
+}
+
+func (c *Cave) Drop(pt Point) Point {
+	for {
+		newPt := Point{pt.X, pt.Y + 1}
+		if newPt.Y > c.Max.Y {
+			return newPt
+		}
+		if c.Cells[newPt] == Empty {
+			pt = newPt
+			continue
+		}
+		newPt.X--
+		if c.Cells[newPt] == Empty {
+			pt = newPt
+			continue
+		}
+		newPt.X += 2
+		if c.Cells[newPt] == Empty {
+			pt = newPt
+			continue
+		}
+		c.Cells[pt] = Sand
+		return pt
+	}
+}
+
+func (c *Cave) Parse(lines []string) {
+	linepat := regexp.MustCompile(`[0-9]+,[0-9]+`)
+	for _, l := range lines {
+		pts := linepat.FindAllString(l, -1)
+		var points []Point
+		for _, p := range pts {
+			values := strings.Split(p, ",")
+			x, _ := strconv.Atoi(values[0])
+			y, _ := strconv.Atoi(values[1])
+			points = append(points, Point{x, y})
+		}
+		for i := 1; i < len(points); i++ {
+			c.DrawWall(points[i-1], points[i])
+		}
+	}
+}
+
+func part1(lines []string) {
+	c := NewCave()
+	origin := Point{500, 0}
+	c.Parse(lines)
+	c.CheckLimits(origin)
+	c.Cells[origin] = Origin
+	c.Print()
+
+	grains := 0
+	for {
+		pt := c.Drop(origin)
+		if pt.Y > c.Max.Y {
+			break
+		}
+		grains++
+	}
+	c.Print()
+	fmt.Println(grains)
+}
+
+func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(b), "\n")
+	part1(lines)
+}
