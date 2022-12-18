0732ecedfc4a7adf608a100d0a306f1e06183ab4
Kent Quirk <kentquirk@honeycomb.io>
2022-12-18 00:08:08 -0500

    Solve part 2, day 17

--- day17/go.mod
+++ day17/go.mod
@@ -1,3 +1,5 @@
 module github.com/kentquirk/aoc2022/day17
 
 go 1.19
+
+require github.com/dgryski/go-wyhash v0.0.0-20191203203029-c4841ae36371
--- day17/main.go
+++ day17/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/dgryski/go-wyhash"
 )
 
 type RockSpace struct {
-	Contents [][]byte
+	Contents []byte
 	Width    int
+}
+
+// converts a string like "..#.#" to a left-justified byte based on the original
+// string length (in this case, the result is 00101000)
+func toBinary(s string) byte {
+	r := strings.NewReplacer(".", "0", "#", "1")
+	s = r.Replace(s)
+	b, _ := strconv.ParseUint(s, 2, 8)
+	return byte(b << (8 - len(s)))
 }
 
 func RS(a ...string) RockSpace {
 	rs := RockSpace{Width: len(a[0])}
 	for _, s := range a {
-		rs.Contents = append(rs.Contents, []byte(s))
+		rs.Contents = append(rs.Contents, toBinary(s))
 	}
 	return rs
 }
@@ -25,13 +36,32 @@ func RS(a ...string) RockSpace {
 // Returns the height of the highest row that contains a nonempty cell
 func (r *RockSpace) Height() int {
 	for i := len(r.Contents) - 1; i >= 0; i-- {
-		for _, b := range r.Contents[i] {
-			if b != byte('.') {
-				return i + 1
-			}
+		if r.Contents[i] != 0 {
+			return i + 1
 		}
 	}
 	return 0
+}
+
+// Calculates a hash for the top of the tower by accumulating it for
+// the values of the top slices until there has been a rock in every
+// location. The hash also includes the current wind and rock indices; when
+// the hash repeats, we've got a repeated situation.
+func (r *RockSpace) Hash(windIx int, rockIx int) uint64 {
+	key := []byte{byte((windIx >> 8) & 0xFF), byte(windIx & 0xFF), byte(rockIx)}
+	var allbits byte = 0
+	for i := len(r.Contents) - 1; i >= 0; i-- {
+		b := r.Contents[i]
+		if b == 0 {
+			continue
+		}
+		key = append(key, b)
+		allbits |= b
+		if allbits&0xFE == 0xFE {
+			break
+		}
+	}
+	return wyhash.Hash(key, 0x14534fe78bc)
 }
 
 // returns true if a rock in the given position intersects the sides, floor, or
@@ -54,10 +84,9 @@ func (r *RockSpace) Collides(rock RockSpace, x int, y int) bool {
 			// if it hasn't collided yet, it can't possibly do it now
 			return false
 		}
-		for rockX := 0; rockX < len(rock.Contents[rockY]); rockX++ {
-			if rock.Contents[rockY][rockX] == byte('#') && r.Contents[spaceY][x+rockX] == byte('#') {
-				return true
-			}
+		rb := rock.Contents[rockY] >> x
+		if rb&r.Contents[spaceY] != 0 {
+			return true
 		}
 	}
 	return false
@@ -66,22 +95,21 @@ func (r *RockSpace) Collides(rock RockSpace, x int, y int) bool {
 // Places a rock that's passed the collide function already
 func (r *RockSpace) Place(rock RockSpace, x int, y int) {
 	for y+len(rock.Contents) >= len(r.Contents) {
-		r.Contents = append(r.Contents, bytes.Repeat([]byte{'.'}, r.Width))
+		r.Contents = append(r.Contents, 0)
 	}
 	for rockY := 0; rockY < len(rock.Contents); rockY++ {
-		for rockX := 0; rockX < len(rock.Contents[rockY]); rockX++ {
-			if rock.Contents[rockY][rockX] == byte('#') {
-				r.Contents[y+rockY][x+rockX] = byte('#')
-			}
-		}
+		r.Contents[y+rockY] |= rock.Contents[rockY] >> x
 	}
 }
 
 func (r *RockSpace) Print() {
 	for i := len(r.Contents) - 1; i >= 0; i-- {
-		fmt.Printf("%s\n", r.Contents[i])
+		b := r.Contents[i]
+		rep := strings.NewReplacer("0", ".", "1", "#")
+		s := fmt.Sprintf("%08b", b)
+		fmt.Printf("%s\n", rep.Replace(s)[:r.Width])
 	}
-	fmt.Printf("%s\n", strings.Repeat("-", r.Width))
+	fmt.Printf("%s\n\n", strings.Repeat("-", r.Width))
 }
 
 var rocks = []RockSpace{
@@ -92,16 +120,42 @@ var rocks = []RockSpace{
 	RS("##", "##"),
 }
 
+type hashState struct {
+	RockCount int
+	Height    int
+}
+
 func part1(breeze string, iterations int) int {
+	hashes := make(map[uint64]hashState)
+	var heightOffset = 0
+
 	chamber := RockSpace{Width: 7}
 	windIx := 0
-	for rockIx := 0; rockIx < iterations; rockIx++ {
-		rock := rocks[rockIx%len(rocks)]
+	doHashes := true
+	for rockCount := 0; rockCount < iterations; rockCount++ {
+		rockIx := rockCount % len(rocks)
+		if doHashes {
+			h := chamber.Hash(windIx, rockIx)
+			state := hashState{RockCount: rockCount, Height: chamber.Height()}
+			if prevState, ok := hashes[h]; ok {
+				nrocks := state.RockCount - prevState.RockCount
+				deltaHeight := state.Height - prevState.Height
+				fmt.Printf("Dup at %v, prev %v, nrocks = %d, deltaH = %d\n", state, prevState, nrocks, deltaHeight)
+				iterationsLeft := iterations - rockCount
+				rockCount += (iterationsLeft / nrocks) * nrocks
+				heightOffset = deltaHeight * (iterationsLeft / nrocks)
+				doHashes = false
+			}
+			hashes[h] = state
+		}
+
+		rock := rocks[rockIx]
 		dropY := chamber.Height() + 3
 		dropX := 2
 		for {
-			wind := breeze[windIx%len(breeze)]
+			wind := breeze[windIx]
 			windIx++
+			windIx = windIx % len(breeze)
 			switch wind {
 			case '<':
 				if !chamber.Collides(rock, dropX-1, dropY) {
@@ -123,31 +177,7 @@ func part1(breeze string, iterations int) int {
 		}
 		// chamber.Print()
 	}
-	return chamber.Height()
-}
-
-func part2(breeze string) int {
-	const iterations = 1_000_000_000_000
-	// we need the least common multiple between the length of breeze
-	// and 5, the number of rocks we're repeatedly dropping.
-	// We need to drop that many rocks, measure the height, drop
-	// that many again, measure the change in height, and then figure out where
-	// the appropriate number of iterations will land us based on the modulus of
-	// iterations and the lcm.
-	lcm := len(breeze)
-	if lcm%len(rocks) != 0 {
-		lcm *= len(rocks)
-	}
-	if lcm%7 != 0 { // width of the chamber
-		lcm *= 7
-	}
-	for i := 0; i < 10; i++ {
-		h1 := part1(breeze, lcm*(i+1))
-		h2 := part1(breeze, lcm*(i+2))
-		delta := h2 - h1
-		fmt.Println(h1, h2, delta)
-	}
-	return lcm
+	return chamber.Height() + heightOffset
 }
 
 func main() {
@@ -161,5 +191,5 @@ func main() {
 	}
 	lines := strings.Split(string(b), "\n")
 	fmt.Println(part1(lines[0], 2022))
-	fmt.Println(part2(lines[0]))
+	fmt.Println(part1(lines[0], 1_000_000_000_000))
 }
