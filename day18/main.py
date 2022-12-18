2f0ec2db6d4e3e9995c9afe6d62273d49b37c6ae
Kent Quirk <kentquirk@honeycomb.io>
2022-12-18 11:07:48 -0500

    Solve day 18, part 1

---
+++ day18/main.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+import itertools
+import collections
+
+def cube(l):
+    return [10*int(x) for x in l.split(',')]
+
+def faces(cube):
+    return [
+        ('x', cube[0]-5, cube[1], cube[2]),
+        ('x', cube[0]+5, cube[1], cube[2]),
+        ('y', cube[0], cube[1]-5, cube[2]),
+        ('y', cube[0], cube[1]+5, cube[2]),
+        ('z', cube[0], cube[1], cube[2]-5),
+        ('z', cube[0], cube[1], cube[2]+5),
+    ]
+
+openfaces=set()
+def collectFace(f):
+    if f in openfaces:
+        openfaces.remove(f)
+    else:
+        openfaces.add(f)
+
+def minmax(r):
+    return min(r), max(r)
+
+if __name__ == "__main__":
+    f = open("./input.txt")
+    lines = f.readlines()
+    cubes = [cube(l.strip()) for l in lines]
+    for c in cubes:
+        for f in faces(c):
+            collectFace(f)
+    count = len(openfaces)
+    print(count)
